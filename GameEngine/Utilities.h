cd8c441d6f9cc4224eda01299594852d7dc7fff9
Vlmspl <bilousvlad2020@gmail.com>
2025-04-12 19:12:49 +0300

    Base of geometry for future volume rendering

--- GameEngine/Utilities.h
+++ GameEngine/Utilities.h
@@ -10,45 +10,52 @@ typedef glm::mat4x4 Matrix4d;
 
 
 enum ApplicationStatusCode {
-  SUCCESS = 0,
-  WINDOW_INITIALIZATION_FAIL = 1,
+	SUCCESS = 0,
+	WINDOW_INITIALIZATION_FAIL = 1,
 
-  UNKNOWN_ERROR = -1
+	UNKNOWN_ERROR = -1
 };
 
 
-float skyboxVertices[] =
-{
-	//   Coordinates
-	-1.0f, -1.0f,  1.0f,//        7--------6
-	 1.0f, -1.0f,  1.0f,//       /|       /|
-	 1.0f, -1.0f, -1.0f,//      4--------5 |
-	-1.0f, -1.0f, -1.0f,//      | |      | |
-	-1.0f,  1.0f,  1.0f,//      | 3------|-2
-	 1.0f,  1.0f,  1.0f,//      |/       |/
-	 1.0f,  1.0f, -1.0f,//      0--------1
-	-1.0f,  1.0f, -1.0f
+inline float volumeBoxPosition[] = {
+	1.0, 1.0, -1.0,
+	1.0, -1.0, -1.0,
+	1.0, 1.0, 1.0,
+	1.0, -1.0, 1.0,
+	-1.0, 1.0, -1.0,
+	-1.0, -1.0, -1.0,
+	-1.0, 1.0, 1.0,
+	-1.0, -1.0, 1.0,
+
 };
 
-unsigned int skyboxIndices[] =
-{
-	// Right
-	1, 2, 6,
-	6, 5, 1,
-	// Left
-	0, 4, 7,
-	7, 3, 0,
-	// Top
-	4, 5, 6,
-	6, 7, 4,
-	// Bottom
-	0, 3, 2,
-	2, 1, 0,
-	// Back
-	0, 1, 5,
-	5, 4, 0,
-	// Front
-	3, 7, 6,
-	6, 2, 3
+inline float volumeBoxUVW[] = {
+    1.0, 1.0, 1.0,
+	1.0, 0.0, 1.0,
+	1.0, 1.0, 0.0,
+	1.0, 0.0, 0.0,
+	0.0, 1.0, 1.0,
+	0.0, 0.0, 1.0,
+	0.0, 1.0, 0.0,
+	0.0, 0.0, 0.0
 };
 
+inline unsigned int volumeBoxIndices[] = {
+	4, 2, 0,
+	2, 7, 3,
+
+	6, 5, 7,
+	1, 7, 5,
+
+	0, 3, 1,
+	4, 1, 5,
+
+	4, 6, 2,
+	2, 6, 7,
+
+	6, 4, 5,
+	1, 3, 7,
+
+	0, 2, 3,
+	4, 0, 1
+};
